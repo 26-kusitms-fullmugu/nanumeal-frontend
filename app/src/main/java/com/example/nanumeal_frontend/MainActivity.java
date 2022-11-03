3c6f51d5f68239df1d98d99bcc3f8a8c27647791
MinseoShindor <tlsalstj01@duksung.ac.kr>
2022-11-03 14:44:15 +0900

    BottomNavi + splash 추가

--- app/src/main/java/com/example/nanumeal_frontend/MainActivity.java
+++ app/src/main/java/com/example/nanumeal_frontend/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.nanumeal_frontend;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
+
+
 }
+
