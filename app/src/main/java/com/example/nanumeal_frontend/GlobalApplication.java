39da2d276293f3ba480e93db4739e78edc9e3f02
MinseoShindor <tlsalstj01@duksung.ac.kr>
2022-11-26 06:28:46 +0900

    final push

--- app/src/main/java/com/example/nanumeal_frontend/GlobalApplication.java
+++ app/src/main/java/com/example/nanumeal_frontend/GlobalApplication.java
@@ -3,6 +3,8 @@ package com.example.nanumeal_frontend;
 import android.app.Application;
 
 //import com.kakao.sdk.common.KakaoSdk;
+//
+//import com.kakao.sdk.common.KakaoSdk;
 
 public class GlobalApplication extends Application {
     private static GlobalApplication instance;
